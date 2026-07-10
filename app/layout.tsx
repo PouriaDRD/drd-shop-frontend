7e6ad96c668031e196d019adc225244636f8434c
pouria-drd <pouriadrd@gmail.com>
2026-07-10 22:19:48 +0330

    improved og

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,7 +51,7 @@ export const metadata: Metadata = {
 
 	applicationName: "DRD Shop",
 
-	metadataBase: new URL("https://shop.pouria-drd.ir"),
+	metadataBase: new URL("https://shop2.pouria-drd.ir"),
 
 	creator: "Pouria Darandi",
 	publisher: "DRD Shop",
@@ -66,25 +66,25 @@ export const metadata: Metadata = {
 	openGraph: {
 		type: "website",
 		locale: "fa_IR",
-		url: "https://shop.pouria-drd.ir",
+		url: "https://shop2.pouria-drd.ir",
 		siteName: "DRD Shop",
 		title: "دی‌آردی شاپ",
 		description:
 			"دی‌آردی شاپ ارائه‌دهنده سرویس‌های VPN پرسرعت، پایدار و اقتصادی برای کاربران ایران با پشتیبانی حرفه‌ای.",
-		// images: [
-		// 	{
-		// 		url: "https://shop.pouria-drd.ir/images/og/opengraph-image.png",
-		// 		width: 600,
-		// 		height: 315,
-		// 		alt: "دی‌آردی شاپ",
-		// 	},
-		// 	{
-		// 		url: "https://shop2.pouria-drd.ir/images/twitter-card.png",
-		// 		width: 600,
-		// 		height: 315,
-		// 		alt: "دی‌آردی شاپ",
-		// 	},
-		// ],
+		images: [
+			// {
+			// 	url: "https://shop.pouria-drd.ir/images/og/opengraph-image.png",
+			// 	width: 600,
+			// 	height: 315,
+			// 	alt: "دی‌آردی شاپ",
+			// },
+			{
+				url: "https://shop2.pouria-drd.ir/images/twitter-card.png",
+				width: 600,
+				height: 315,
+				alt: "دی‌آردی شاپ",
+			},
+		],
 	},
 
 	twitter: {
@@ -94,24 +94,24 @@ export const metadata: Metadata = {
 		title: "دی‌آردی شاپ | خرید VPN پرسرعت و ارزان",
 		description:
 			"سرویس VPN پرسرعت، پایدار و اقتصادی دی‌آردی شاپ برای کاربران ایران.",
-		// images: [
-		// 	{
-		// 		url: "https://shop.pouria-drd.ir/images/twitter-card.png",
-		// 		width: 600,
-		// 		height: 315,
-		// 		alt: "دی‌آردی شاپ",
-		// 	},
-		// 	{
-		// 		url: "https://shop2.pouria-drd.ir/images/twitter-card.png",
-		// 		width: 600,
-		// 		height: 315,
-		// 		alt: "دی‌آردی شاپ",
-		// 	},
-		// ],
+		images: [
+			// {
+			// 	url: "https://shop.pouria-drd.ir/images/twitter-card.png",
+			// 	width: 600,
+			// 	height: 315,
+			// 	alt: "دی‌آردی شاپ",
+			// },
+			{
+				url: "https://shop2.pouria-drd.ir/images/twitter-card.png",
+				width: 600,
+				height: 315,
+				alt: "دی‌آردی شاپ",
+			},
+		],
 	},
 
 	alternates: {
-		canonical: "https://shop.pouria-drd.ir",
+		canonical: "https://shop2.pouria-drd.ir",
 	},
 
 	robots: {
@@ -138,7 +138,7 @@ export const metadata: Metadata = {
 			name: "DRD Shop",
 			alternateName: "دی‌آردی شاپ",
 
-			url: "https://shop.pouria-drd.ir",
+			url: "https://shop2.pouria-drd.ir",
 
 			description:
 				"دی‌آردی شاپ ارائه‌دهنده سرویس‌های VPN پرسرعت، پایدار و اقتصادی برای کاربران ایران با پشتیبانی حرفه‌ای.",
