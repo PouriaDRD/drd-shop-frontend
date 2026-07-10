f305f37b37c869fe98c9d9b46f027475d0434139
pouria-drd <pouriadrd@gmail.com>
2026-07-10 23:17:03 +0330

    improved og

--- components/pages/landing/landing.data.ts
+++ components/pages/landing/landing.data.ts
@@ -13,7 +13,7 @@ export type ServerStat = {
 };
 
 export const liveServers: ServerStat[] = [
-	{ id: "de-fra", country: "آلمان", city: "فرانکفورت", ping: 74, load: 73 },
+	{ id: "de-fra", country: "آلمان", city: "فرانکفورت", ping: 74, load: 47 },
 	// { id: "nl-ams", country: "هلند", city: "آمستردام", ping: 80, load: 28 },
 	// { id: "tr-ist", country: "ترکیه", city: "استانبول", ping: 42, load: 12 },
 	// { id: "us-nyc", country: "آمریکا", city: "نیویورک", ping: 142, load: 9 },
